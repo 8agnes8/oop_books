f607b6b1c5575008926c08e38dc84a7ea387be17
8agnes8 <92390983+8agnes8@users.noreply.github.com>
2021-12-08 11:07:36 +0200

    add all

---
+++ app.js
@@ -0,0 +1,42 @@
+//app objects
+const ui = new UI()
+
+//user input form
+const form = document.querySelector('#book-form')
+form.addEventListener('submit' , addBook)
+
+
+
+//addbook function
+function addBook(event){
+    //get form data from input
+    const title = ui.title.value
+    const author = ui.author.value
+    const isbn = ui.isbn.value
+    //create book object with user data
+    const book = new Book(title, author, isbn)
+
+
+    //add book data to UI and show it
+    ui.addBook(book)
+
+
+    const tr = document.createElement('tr')
+    const trContent = `<td>${book.title}</td>
+     <td>${book.author}</td>
+     <td>${book.isbn}</td>
+     <td><a href="#">X</a></td>
+   `
+    tr.innerHTML = trContent
+    const bookList = document.querySelector('#book-list')
+    bookList.appendChild(tr)
+
+
+    //clear from input
+    ui.title.value = ''
+    ui.author.value = ''
+    ui.isbn.value = ''
+    event.preventDefault()
+
+
+}
